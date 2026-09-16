342a81d5e5157364903e85b5bb3e4f676b0a8f34
susanlcline <78167697+susanlcline@users.noreply.github.com>
2026-09-16 14:05:55 -0700

    Updated yarn and added react datatable

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,28 +1,177 @@
 import type { Route } from "./+types/home";
-import { Search } from "@carbon/react";
+import { Search, Table, TableHead, TableRow, TableHeader, TableBody, TableCell } from "@carbon/react";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Bay Oaks U16G ECNL-RL Player Highlights" },
+    { name: "description", content: "Highlights for players - add and view" },
   ];
 }
 
 export default function Home() {
   return (
-    <div
-      style={{
-        width: 800
-      }}
-    >
-      <Search
-        closeButtonLabelText="Clear search input"
-        id="search-default-1"
-        labelText="Site search"
-        placeholder="Placeholder text"
-        size="md"
-        type="search"
-      />
+    <div>
+      <div
+        style={{
+          width: 800
+        }}
+      >
+        <Search
+          closeButtonLabelText="Clear search input"
+          id="search-default-1"
+          labelText="Site search"
+          placeholder="Placeholder text"
+          size="md"
+          type="search"
+        />
+      </div>
+      <div>
+        <Table
+          aria-label="sample table"
+          size="lg"
+        >
+          <TableHead>
+            <TableRow>
+              <TableHeader>
+                Name
+              </TableHeader>
+              <TableHeader>
+                Rule
+              </TableHeader>
+              <TableHeader>
+                Status
+              </TableHeader>
+              <TableHeader>
+                Other
+              </TableHeader>
+              <TableHeader>
+                Example
+              </TableHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                Load Balancer 1
+              </TableCell>
+              <TableCell>
+                Round robin
+              </TableCell>
+              <TableCell>
+                Starting
+              </TableCell>
+              <TableCell>
+                Test
+              </TableCell>
+              <TableCell>
+                22
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Load Balancer 2
+              </TableCell>
+              <TableCell>
+                DNS delegation
+              </TableCell>
+              <TableCell>
+                Active
+              </TableCell>
+              <TableCell>
+                Test
+              </TableCell>
+              <TableCell>
+                22
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Load Balancer 3
+              </TableCell>
+              <TableCell>
+                Round robin
+              </TableCell>
+              <TableCell>
+                Disabled
+              </TableCell>
+              <TableCell>
+                Test
+              </TableCell>
+              <TableCell>
+                22
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Load Balancer 4
+              </TableCell>
+              <TableCell>
+                Round robin
+              </TableCell>
+              <TableCell>
+                Disabled
+              </TableCell>
+              <TableCell>
+                Test
+              </TableCell>
+              <TableCell>
+                22
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Load Balancer 5
+              </TableCell>
+              <TableCell>
+                Round robin
+              </TableCell>
+              <TableCell>
+                Disabled
+              </TableCell>
+              <TableCell>
+                Test
+              </TableCell>
+              <TableCell>
+                22
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Load Balancer 6
+              </TableCell>
+              <TableCell>
+                Round robin
+              </TableCell>
+              <TableCell>
+                Disabled
+              </TableCell>
+              <TableCell>
+                Test
+              </TableCell>
+              <TableCell>
+                22
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Load Balancer 7
+              </TableCell>
+              <TableCell>
+                Round robin
+              </TableCell>
+              <TableCell>
+                Disabled
+              </TableCell>
+              <TableCell>
+                Test
+              </TableCell>
+              <TableCell>
+                22
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
+import react from '@vitejs/plugin-react';
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [reactRouter(), react()],
   resolve: {
     tsconfigPaths: true,
   },
+    build: {
+    rollupOptions: {
+      // Add the package here to fix the error
+      external: [
+        /node_modules\/@carbon\/react/
+      ]
+    }
+  }
 });
